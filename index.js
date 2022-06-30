322531b0e9945b6128e239da1ff85159b44cf1ea
malfell <malfellows13@gmail.com>
2022-06-29 23:52:23 -0500

    set up files, Express, and Mongoose

---
+++ index.js
@@ -0,0 +1,31 @@
+// MODULES AND GLOBALS
+// Require Express
+const express = require('express');
+// Require Mongoose
+const mongoose = require('mongoose');
+
+//CONFIGURATION
+//require dotenv
+require('dotenv').config();
+//initialize the app object
+const app = express();
+
+// mongoose can be called anywhere after the required mongoose
+// The first argument that connect takes is the Mongo URI. 
+// The second argument contains optional properties that get rid 
+// of deprecation warnings.
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+
+// Middleware
+//parses JSON and puts it in req.body
+app.use(express.json());
+
+// HOMEPAGE ROUTE
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// LISTEN FOR CONNECTIONS
+app.listen(3000)
